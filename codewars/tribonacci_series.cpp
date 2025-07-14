b51be6d938ed9b376a23eea63eedc3e85d66326c
Cesar1R <106183934+Cesar1R@users.noreply.github.com>
2025-07-13 22:33:50 -0600

    Update tribonacci_series.cpp

--- codewars/tribonacci_series.cpp
+++ codewars/tribonacci_series.cpp
@@ -3,17 +3,10 @@ std::vector<int> tribonacci(std::vector<int> signature, int n){
 	std::vector<int> result(signature.begin(), signature.begin()+std::min(n, 3)); 
 	
 	if(n>3){ 	
-		int i, k, term=0;
-
-		for(i=0; i<3; i++){
-			term +=signature[i];
-		}
-	
+		int i=0, k, term=signature[0] + signature[1] + signature[2];
 		result.push_back(term);
-		i=0;
 		for(k=0; k<n-4; k++){
-			term *=2;
-			term -= result[i++];
+			term *=2; term -= result[i++];
 			result.push_back(term);
 		}	
 	}  
